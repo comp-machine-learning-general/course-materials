1b7b5cd09f499544ba00526c193b4a8cd6fbc973
Katherine M. Kinnaird <kkinnaird@smith.edu>
2019-11-15 14:18:12 -0500

    adding notes to lab 11

--- Labs/Lab19-Benchmarking/mprun_demo.py
+++
@@ -1,9 +0,0 @@
-
-from sklearn.ensemble import RandomForestClassifier
-import numpy as np
-from numpy import linalg as LA
-import pandas as pd
-
-def tune_fit(in_vars,classes):
-    grove = RandomForestClassifier(n_estimators=10, max_features = 3, max_depth=2, random_state=0)
-    grove.fit(in_vars,classes)
